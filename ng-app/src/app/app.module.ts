dc2f335240d97c9c765a06e96024562c0ff6af45
mogit2020 <mohibullah.kamal@gmail.com>
2019-06-17 19:01:10 +0200

    add main-window component

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWindowComponent } from './main-window/main-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWindowComponent
   ],
   imports: [
     BrowserModule,
