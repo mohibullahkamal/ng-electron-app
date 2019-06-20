7f7937b63939bc1a7c964c7846eeb7804ba86f93
mogit2020 <mohibullah.kamal@gmail.com>
2019-06-20 14:38:40 +0200

    add user component

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainWindowComponent } from './main-window/main-window.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainWindowComponent
+    MainWindowComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
